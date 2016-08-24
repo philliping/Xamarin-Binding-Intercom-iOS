24634f1ea83e64b845d2a361a245c5a604f66b15
Phillip Ingelmo <phillip.ingelmo@gmail.com>
2016-08-24 12:29:45 -0400

    Initial commit

---
+++ Structs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Intercom.iOS
+{
+	[Native]
+	public enum ICMPreviewPosition : long
+	{
+		BottomLeft = 0,
+		BottomRight = 1,
+		TopLeft = 2,
+		TopRight = 3
+	}
+}
